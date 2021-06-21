7e9720e7b356ece78256bb258d760e4537f34cd4
Shawn <38536410+GuYuanjie@users.noreply.github.com>
2021-06-21 17:18:42 +0800

    Add files via upload

---
+++ titlebar/maintitlebar.h
@@ -0,0 +1,34 @@
+﻿/*****************************************
+ * 作者: YYC
+ * 日期: 2020-04-26
+ * 功能：主标题栏，内含有窗体最大化，最小化，关闭
+ * 以及窗口的Title，以及名称
+ * ***************************************/
+#ifndef MAINTITLEBAR_H
+#define MAINTITLEBAR_H
+
+#include <QWidget>
+#include "titlebar/titlebar.h"
+
+namespace Ui {
+class MainTitleBar;
+}
+
+class MainTitleBar : public TitleBar
+{
+    Q_OBJECT
+
+public:
+    explicit MainTitleBar(QWidget *parent = 0);
+    ~MainTitleBar();
+
+private slots:
+    void on_pushButtonClose_clicked();
+    void on_pushButtonMin_clicked();
+    void on_pushButtonNormalMax_clicked();
+
+private:
+    Ui::MainTitleBar *ui;
+};
+
+#endif // MAINTITLEBAR_H
