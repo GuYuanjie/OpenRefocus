c89a4a5debade9dfcd49a7ed83382b94e1f6be9a
Shawn <38536410+GuYuanjie@users.noreply.github.com>
2021-06-21 17:17:40 +0800

    Add files via upload

---
+++ systemtray/systemtray.cpp
@@ -0,0 +1,77 @@
+﻿/*****************************************
+ * 作者: YYC
+ * 日期: 2020-04-26
+ * 功能：系统托盘，未进行优化，只有最简单的
+ * 功能，后续会继续优化
+ * ***************************************/
+#include "systemtray.h"
+#include "globaldef.hpp"
+#include <QDebug>
+#include <QIcon>
+#include <QUrl>
+#include <QDesktopServices>
+
+
+SystemTray::SystemTray(QWidget * parent)
+    :QSystemTrayIcon(parent)
+{
+    this->createAction();
+    this->addAction();
+}
+
+
+SystemTray::~SystemTray()
+{
+
+}
+
+void SystemTray::addAction()
+{
+    myMenu->addAction(showWidget);
+    myMenu->addAction(minWidget);
+    myMenu->addAction(aboutSoftWare);
+    myMenu->addAction(exitSoftWare);
+}
+
+
+void SystemTray::createAction()
+{
+    myMenu = new QMenu();
+    showWidget = new QAction(tr("Display the window"), this);
+    minWidget = new QAction(tr("Hide the window"), this);
+    aboutSoftWare = new QAction(tr("About the application"), this);
+    exitSoftWare = new QAction(tr("Quit the application"), this);
+
+    this->setIcon(QIcon(GlobalSpace::LOGO_PATH));
+    this->setToolTip(tr("OpenRefocus"));
+
+    connect(exitSoftWare, SIGNAL(triggered(bool)), qApp, SLOT(quit()));
+    connect(aboutSoftWare, SIGNAL(triggered(bool)), this, SLOT(aboutSlot()));
+
+    this->setContextMenu(myMenu);
+    this->show();
+}
+
+
+QAction *SystemTray::getAboutSoftWare() const
+{
+    return aboutSoftWare;
+}
+
+
+void SystemTray::aboutSlot()
+{
+    QDesktopServices::openUrl(QUrl("https://www.baidu.com"));
+}
+
+
+QAction *SystemTray::getMinWidget() const
+{
+    return minWidget;
+}
+
+
+QAction *SystemTray::getShowWidget() const
+{
+    return showWidget;
+}
---
+++ systemtray/systemtray.h
@@ -0,0 +1,52 @@
+﻿/*****************************************
+ * 作者: YYC
+ * 日期: 2020-04-26
+ * 功能：系统托盘，未进行优化，只有最简单的
+ * 功能，后续会继续优化
+ * ***************************************/
+
+#ifndef SYSTEMTRAY_H
+#define SYSTEMTRAY_H
+
+#if _MSC_VER >= 1600
+#pragma execution_character_set("utf-8")
+#endif
+
+#include<QSystemTrayIcon>
+#include<QAction>
+#include<QApplication>
+#include<QMenu>
+#include<QWidget>
+#include<QObject>
+
+class SystemTray : public QSystemTrayIcon
+{
+    Q_OBJECT
+public:
+    explicit SystemTray(QWidget * parent = 0);
+    ~SystemTray();
+
+    void addAction();
+    void createAction();
+
+public:
+    QAction *getShowWidget() const;
+    QAction *getMinWidget() const;
+    QAction *getAboutSoftWare() const;
+
+signals:
+    void showMainWindow();
+    void showAboutUs();
+
+private slots:
+    void aboutSlot();
+
+private:
+    QMenu *myMenu;
+    QAction* showWidget;
+    QAction* minWidget;
+    QAction* aboutSoftWare;
+    QAction* exitSoftWare;
+};
+
+#endif // SYSTEMTRAY_H
